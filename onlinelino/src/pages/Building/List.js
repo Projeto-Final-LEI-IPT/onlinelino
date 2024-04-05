034aa93048d98f34c29977da305d284b1bc8df43
Ana Grácio <64323829+anagracio@users.noreply.github.com>
2024-04-05 18:14:44 +0100

    Added a list of buildings

--- onlinelino/src/pages/Building/List.js
+++ onlinelino/src/pages/Building/List.js
@@ -2,6 +2,7 @@ import React from "react";
 import NavbarHome from "../../components/NavbarHome";
 import NavbarBuilding from "../../components/NavbarBuilding";
 import Container from "react-bootstrap/esm/Container";
+import data from '../../assets/data.json';
 
 class ListIndex extends React.Component {
     // constructor(props) {
@@ -17,6 +18,18 @@ class ListIndex extends React.Component {
                 <br />
                 <Container>
                     <p>Lista</p>
+                        {data.map(building => (
+                            <div key={building.id}>
+                                <div style={{ display: 'flex', alignItems: 'center' }}>
+                                    <img src={`../${building.images[0]}`} alt={building.title} style={{ maxWidth: '200px', marginRight: '10px', maxHeight: '150px' }} />
+                                    <div>
+                                        <h4>{building.title}</h4>
+                                        <p>{building.info}</p>
+                                    </div>
+                                </div>
+                                <br />
+                            </div>
+                        ))}
                 </Container>
             </>
         );
