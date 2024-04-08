0bd97c5ac79308ebd716ebf41acf12e78a300a58
Ana Grácio <64323829+anagracio@users.noreply.github.com>
2024-04-08 15:08:41 +0100

    Added link from list to details

--- onlinelino/src/pages/Building/List.js
+++ onlinelino/src/pages/Building/List.js
@@ -2,6 +2,7 @@ import React from "react";
 import NavbarHome from "../../components/NavbarHome";
 import NavbarBuilding from "../../components/NavbarBuilding";
 import Container from "react-bootstrap/esm/Container";
+import { Link } from "react-router-dom"; // Import Link from React Router
 import data from '../../assets/data.json';
 
 class ListIndex extends React.Component {
@@ -17,9 +18,10 @@ class ListIndex extends React.Component {
                 <NavbarBuilding />
                 <br />
                 <Container>
-                    <p>Lista</p>
-                        {data.map(building => (
-                            <div key={building.id}>
+                    <h4>Lista de Obras</h4>
+                    {data.map(building => (
+                        <div key={building.id}>
+                            <Link key={building.id} to={`/obra/${building.id}`} style={{ textDecoration: 'none', color: 'inherit' }}> {/* Wrap each item with Link */}
                                 <div style={{ display: 'flex', alignItems: 'center' }}>
                                     <img src={`../${building.images[0]}`} alt={building.title} style={{ maxWidth: '200px', marginRight: '10px', maxHeight: '150px' }} />
                                     <div>
@@ -27,9 +29,10 @@ class ListIndex extends React.Component {
                                         <p>{building.info}</p>
                                     </div>
                                 </div>
-                                <br />
-                            </div>
-                        ))}
+                            </Link>
+                            <br />
+                        </div>
+                    ))}
                 </Container>
             </>
         );
