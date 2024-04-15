403edbcc3e75fc7ebf7346b98a842b0885b673cc
Ana Grácio <64323829+anagracio@users.noreply.github.com>
2024-04-15 15:46:54 +0100

    Changed text in navbar to support translation

--- onlinelino/src/components/NavbarBiography.js
+++ onlinelino/src/components/NavbarBiography.js
@@ -2,27 +2,23 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { useTranslation } from 'react-i18next';
 
-class NavbarBiography extends React.Component {
-    // constructor(props) {
-    //     super(props);
-    // }
-
-    render() {
-        return (
-            <>
-                <Navbar className="bg" style={{ backgroundColor: '#fff6db' }}>
-                    <Container>
-                        <Nav className="mr-auto">
-                            <Nav.Link href="/biografia" style={{ paddingLeft: '0' }}>GENÉRICA</Nav.Link>
-                            <span className="navbar-text">|</span>
-                            <Nav.Link href="/biografia/sobre">SOBRE O TRABALHO NO MÉDIO TEJO</Nav.Link>
-                        </Nav>
-                    </Container>
-                </Navbar>
-            </>
-        );
-    }
+function NavbarBiography() {
+    const { t } = useTranslation();
+    return (
+        <>
+            <Navbar className="bg" style={{ backgroundColor: '#fff6db' }}>
+                <Container>
+                    <Nav className="mr-auto">
+                        <Nav.Link href="/biografia" style={{ paddingLeft: '0' }}>{t('navbarBiography.generic')}</Nav.Link>
+                        <span className="navbar-text">|</span>
+                        <Nav.Link href="/biografia/sobre">{t('navbarBiography.about')}</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+        </>
+    );
 }
 
 export default NavbarBiography;
--- onlinelino/src/components/NavbarProject.js
+++ onlinelino/src/components/NavbarProject.js
@@ -2,31 +2,28 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { useTranslation } from 'react-i18next';
 
-class NavbarProject extends React.Component {
-    // constructor(props) {
-    //     super(props);
-    // }
-
-    render() {
-        return (
-            <>
-                <Navbar className="bg" style={{ backgroundColor: '#fff6db' }}>
-                    <Container>
-                        <Nav className="mr-auto">
-                            <Nav.Link href="/projeto/descricao" style={{ paddingLeft: '0' }}>DESCRIÇÃO</Nav.Link>
-                            <span className="navbar-text">|</span>
-                            <Nav.Link href="/projeto/bibliografia">BIBLIOGRAFIA</Nav.Link>
-                            <span className="navbar-text">|</span>
-                            <Nav.Link href="/projeto/equipa">EQUIPA</Nav.Link>
-                            <span className="navbar-text">|</span>
-                            <Nav.Link href="/projeto/contactos">CONTACTOS</Nav.Link>
-                        </Nav>
-                    </Container>
-                </Navbar>
-            </>
-        );
-    }
+function NavbarProject() {
+    const { t } = useTranslation();
+    return (
+        <>
+            <Navbar className="bg" style={{ backgroundColor: '#fff6db' }}>
+                <Container>
+                    <Nav className="mr-auto">
+                        <Nav.Link href="/projeto/descricao" style={{ paddingLeft: '0' }}>{t('navbarProject.description')}</Nav.Link>
+                        <span className="navbar-text">|</span>
+                        <Nav.Link href="/projeto/bibliografia">{t('navbarProject.bibliography')}</Nav.Link>
+                        <span className="navbar-text">|</span>
+                        <Nav.Link href="/projeto/equipa">{t('navbarProject.team')}</Nav.Link>
+                        <span className="navbar-text">|</span>
+                        <Nav.Link href="/projeto/contactos">{t('navbarProject.contacts')}</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+        </>
+    );
 }
+
 
 export default NavbarProject;
