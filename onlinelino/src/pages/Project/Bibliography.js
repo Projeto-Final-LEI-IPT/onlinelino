24a57ca5e0e8e9a52c2a61d375015dd587a1f96b
Ana Grácio <64323829+anagracio@users.noreply.github.com>
2024-04-24 19:12:21 +0100

    Update Bibliography.js

--- onlinelino/src/pages/Project/Bibliography.js
+++ onlinelino/src/pages/Project/Bibliography.js
@@ -2,25 +2,40 @@ import React from "react";
 import NavbarHome from "../../components/NavbarHome";
 import NavbarProject from "../../components/NavbarProject";
 import Container from "react-bootstrap/esm/Container";
+import { useTranslation } from 'react-i18next';
 
-class BibliographyIndex extends React.Component {
-    // constructor(props) {
-    //     super(props);
-    // }
-
-    render() {
-        return (
-            <>
-                <NavbarHome />
-                <br />
-                <NavbarProject />
-                <br />
-                <Container>
-                    <p>Bibliografia</p>
-                </Container>
-            </>
-        );
+function BibliographyIndex() {
+    // bibliographyPage.text
+    const { t } = useTranslation('translation', { keyPrefix: 'bibliographyPage.text' });
+    const b = [];
+    for (let i = 0; i < 50; i++) {
+        if (!t([i]).includes("bibliographyPage.text")) {
+            b.push(t([i]));
+        }
     }
+    // bibliographyPage.biography
+    const { t: bibliography } = useTranslation();
+
+    return (
+        <>
+            <NavbarHome />
+            <br />
+            <NavbarProject />
+            <br />
+            <Container>
+                <h4>{bibliography('bibliographyPage.bibliography')}</h4>
+                <br/>
+                <ul>
+                    {b.map((paragraph, index) => (
+                        <>
+                            <li key={index}>{paragraph}</li>
+                            <br />
+                        </>
+                    ))}
+                </ul>
+            </Container>
+        </>
+    );
 }
 
 export default BibliographyIndex;
