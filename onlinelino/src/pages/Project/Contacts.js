4e761c4f044891d126fa5a386bf2380172d81ae0
Ana Grácio <64323829+anagracio@users.noreply.github.com>
2024-04-24 19:30:48 +0100

    Update Contacts.js

--- onlinelino/src/pages/Project/Contacts.js
+++ onlinelino/src/pages/Project/Contacts.js
@@ -2,25 +2,40 @@ import React from "react";
 import NavbarHome from "../../components/NavbarHome";
 import NavbarProject from "../../components/NavbarProject";
 import Container from "react-bootstrap/esm/Container";
+import { useTranslation } from 'react-i18next';
 
-class ContactsIndex extends React.Component {
-    // constructor(props) {
-    //     super(props);
-    // }
-
-    render() {
-        return (
-            <>
-                <NavbarHome />
-                <br />
-                <NavbarProject />
-                <br />
-                <Container>
-                    <p>Contactos</p>
-                </Container>
-            </>
-        );
+function ContactsIndex() {
+    // contactsPage.text
+    const { t: text } = useTranslation('translation', { keyPrefix: 'contactsPage.text' });
+    const b = [];
+    for (let i = 0; i < 50; i++) {
+        if (!text([i]).includes("contactsPage.text")) {
+            b.push(text([i]));
+        }
     }
+    // bibliographyPage.biography
+    const { t } = useTranslation();
+
+    return (
+        <>
+            <NavbarHome />
+            <br />
+            <NavbarProject />
+            <br />
+            <Container>
+                <h4>{t('contactsPage.contacts')}</h4>
+                <br/>
+                <ul>
+                    {b.map((paragraph, index) => (
+                        <>
+                            <li key={index}>{paragraph}</li>
+                            <br />
+                        </>
+                    ))}
+                </ul>
+            </Container>
+        </>
+    );
 }
 
 export default ContactsIndex;
