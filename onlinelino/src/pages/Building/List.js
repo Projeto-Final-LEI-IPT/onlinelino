47c955ffd1c62ab210332070da6aa0e2100e92f0
Ana Grácio <64323829+anagracio@users.noreply.github.com>
2024-04-29 15:51:55 +0100

    Update Building/List.js

--- onlinelino/src/pages/Building/List.js
+++ onlinelino/src/pages/Building/List.js
@@ -2,41 +2,42 @@ import React from "react";
 import NavbarHome from "../../components/NavbarHome";
 import NavbarBuilding from "../../components/NavbarBuilding";
 import Container from "react-bootstrap/esm/Container";
-import { Link } from "react-router-dom"; 
-import data from '../../assets/data.json';
+import { Link } from "react-router-dom";
+import { useTranslation } from 'react-i18next';
 
-class ListIndex extends React.Component {
-    // constructor(props) {
-    //     super(props);
-    // }
+function ListIndex() {
+    const { t } = useTranslation();
 
-    render() {
-        return (
-            <>
-                <NavbarHome />
+    // buildings.[i].title + ", " + buildings.[i].year
+    const list = [];
+    for (let i = 0; i < 50; i++) {
+        if (!t(`buildings.${i}.title`).includes("title") && !t(`buildings.${i}.year`).includes("year")) {
+            list.push(t(`buildings.${i}.title`).toString() + ", " + t(`buildings.${i}.year`).toString());
+        }
+    }
+
+    return (
+        <>
+            <NavbarHome />
+            <br />
+            <NavbarBuilding />
+            <br />
+            <Container>
+                <h4>{t('listPage.title')}</h4>
                 <br />
-                <NavbarBuilding />
-                <br />
-                <Container>
-                    <h4>Lista de Obras</h4>
-                    {data.map(building => (
-                        <div key={building.id}>
-                            <Link key={building.id} to={`/obra/${building.id}`} style={{ textDecoration: 'none', color: 'inherit' }}> {/* Wrap each item with Link */}
-                                <div style={{ display: 'flex', alignItems: 'center' }}>
-                                    <img src={`../${building.images[0]}`} alt={building.title} style={{ maxWidth: '200px', marginRight: '10px', maxHeight: '150px' }} />
-                                    <div>
-                                        <h4>{building.title}</h4>
-                                        <p>{building.info}</p>
-                                    </div>
-                                </div>
+                <ul>
+                    {list.map((paragraph, index) => (
+                        <>
+                            <Link key={index} to={`/obra/${index + 1}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+                                <li key={index}>{paragraph}</li>
                             </Link>
                             <br />
-                        </div>
+                        </>
                     ))}
-                </Container>
-            </>
-        );
-    }
+                </ul>
+            </Container>
+        </>
+    );
 }
 
 export default ListIndex;
