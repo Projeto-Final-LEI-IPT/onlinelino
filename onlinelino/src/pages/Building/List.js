e43dff8a9b2c3dcddef9458f3f533d5af64c2183
Ana Grácio <64323829+anagracio@users.noreply.github.com>
2024-06-17 21:13:54 +0100

    Changed List Style

--- onlinelino/src/pages/Building/List.js
+++ onlinelino/src/pages/Building/List.js
@@ -3,6 +3,7 @@ import NavbarHome from "../../components/NavbarHome";
 import Container from "react-bootstrap/esm/Container";
 import { Link } from "react-router-dom";
 import { useTranslation } from 'react-i18next';
+import '../../style/List.css';
 
 function ListIndex() {
     const { t } = useTranslation();
@@ -12,11 +13,13 @@ function ListIndex() {
     for (let i = 0; i < 50; i++) {
         const title = t(`buildings.${i}.title`);
         const year = t(`buildings.${i}.year`);
+        const year2 = t(`buildings.${i}.year2`);
 
         if (!title.includes("title") && !year.includes("year")) {
             buildings.push({
                 title: title,
-                year: parseInt(year)
+                year: parseInt(year),
+                year2: year+year2
             });
         }
     }
@@ -27,18 +30,18 @@ function ListIndex() {
             <NavbarHome />
             <br />
             <Container>
-                <h4>{t('listPage.title')}</h4>
-                <br />
                 <ul>
                     {buildings.map((paragraph, index) => (
                         <>
                             <Link key={index} to={`/obra/${index + 1}`} style={{ textDecoration: 'none', color: 'inherit' }}>
-                                <li key={index}>{paragraph.title}, {paragraph.year}</li>
+                                <li className="list-item" key={index}>
+                                    <p className="text-year">{paragraph.year2}</p> 
+                                    <p className="text-title">{paragraph.title}</p>
+                                </li>
                             </Link>
                             <br />
                         </>
                     ))}
-                    {/* {buildings} */}
                 </ul>
             </Container>
         </>
