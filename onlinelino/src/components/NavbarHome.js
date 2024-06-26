021534272e0ececc0bd77c69baf614f78a395ff7
Ana Grácio <64323829+anagracio@users.noreply.github.com>
2024-06-26 16:19:15 +0100

    Fixed navbar dropdown and highlighted item

--- onlinelino/src/components/NavbarHome.js
+++ onlinelino/src/components/NavbarHome.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import '../style/Navbar.css';
 import { useTranslation } from 'react-i18next';
 import LanguageSwitcher from './LanguageSwitcher';
@@ -7,10 +7,13 @@ import LanguageSwitcher from './LanguageSwitcher';
 const NavbarHome = () => {
     const [activeDropdown, setActiveDropdown] = useState(null);
     const { t } = useTranslation();
+    const location = useLocation();
 
     const handleDropdownToggle = (index) => {
         setActiveDropdown(index);
     };
+
+    const isActive = (path) => location.pathname.includes(path);
 
     return (
         <>
@@ -26,55 +29,54 @@ const NavbarHome = () => {
             </div>
             <nav className="navbar">
                 <ul className="navbar-list">
-                    <li className="navbar-item">
+                    <li className={`navbar-item ${isActive('/projeto') ? 'active' : ''}`}>
                         <Link className="navbar-button" onClick={() => handleDropdownToggle(0)}>
                             {t('navbarHome.project')}
                         </Link>
-                        <ul className={`dropdown-menu ${activeDropdown === 0 ? 'show' : ''}`}>
+                        <ul className={`dropdown-menu2 ${activeDropdown === 0 ? 'show' : ''}`}>
                             <li>
-                                <Link to="/projeto/descricao" className="dropdown-item">{t('navbarProject.description')}</Link>
+                                <Link to="/projeto/descricao" className="dropdown-item2">{t('navbarProject.description')}</Link>
                             </li>
                             <li>
-                                <Link to="/projeto/bibliografia" className="dropdown-item">{t('navbarProject.bibliography')}</Link>
+                                <Link to="/projeto/bibliografia" className="dropdown-item2">{t('navbarProject.bibliography')}</Link>
                             </li>
                             <li>
-                                <Link to="/projeto/equipa" className="dropdown-item">{t('navbarProject.team')}</Link>
+                                <Link to="/projeto/equipa" className="dropdown-item2">{t('navbarProject.team')}</Link>
                             </li>
                             <li>
-                                <Link to="/projeto/contactos" className="dropdown-item">{t('navbarProject.contacts')}</Link>
+                                <Link to="/projeto/contactos" className="dropdown-item2">{t('navbarProject.contacts')}</Link>
                             </li>
                         </ul>
                     </li>
-                    <li className="navbar-item">
-                        <Link className="navbar-button" onClick={() => handleDropdownToggle(1)}
-                        >
+                    <li className={`navbar-item ${isActive('/biografia') ? 'active' : ''}`}>
+                        <Link className="navbar-button" onClick={() => handleDropdownToggle(1)}>
                             {t('navbarHome.biography')}
                         </Link>
-                        <ul className={`dropdown-menu ${activeDropdown === 1 ? 'show' : ''}`}>
+                        <ul className={`dropdown-menu2 ${activeDropdown === 1 ? 'show' : ''}`}>
                             <li>
-                                <Link to="/biografia" className="dropdown-item">{t('navbarBiography.generic')}</Link>
+                                <Link to="/biografia" className="dropdown-item2">{t('navbarBiography.generic')}</Link>
                             </li>
                             <li>
-                                <Link to="/biografia/sobre" className="dropdown-item">{t('navbarBiography.about')}</Link>
+                                <Link to="/biografia/sobre" className="dropdown-item2">{t('navbarBiography.about')}</Link>
                             </li>
                         </ul>
                     </li>
-                    <li className="navbar-item">
+                    <li className={`navbar-item ${isActive('/obra') ? 'active' : ''}`}>
                         <Link className="navbar-button" onClick={() => handleDropdownToggle(2)}>
                             {t('navbarHome.building')}
                         </Link>
-                        <ul className={`dropdown-menu ${activeDropdown === 2 ? 'show' : ''}`}>
+                        <ul className={`dropdown-menu2 ${activeDropdown === 2 ? 'show' : ''}`}>
                             <li>
-                                <Link to="/obra/detalhes" className="dropdown-item">{t('navbarBuilding.details')}</Link>
+                                <Link to="/obra/detalhes" className="dropdown-item2">{t('navbarBuilding.details')}</Link>
                             </li>
                             <li>
-                                <Link to="/obra/cronologia" className="dropdown-item">{t('navbarBuilding.chronology')}</Link>
+                                <Link to="/obra/cronologia" className="dropdown-item2">{t('navbarBuilding.chronology')}</Link>
                             </li>
                             <li>
-                                <Link to="/obra/mapa" className="dropdown-item">{t('navbarBuilding.map')}</Link>
+                                <Link to="/obra/mapa" className="dropdown-item2">{t('navbarBuilding.map')}</Link>
                             </li>
                             <li>
-                                <Link to="/obra/lista" className="dropdown-item">{t('navbarBuilding.list')}</Link>
+                                <Link to="/obra/lista" className="dropdown-item2">{t('navbarBuilding.list')}</Link>
                             </li>
                         </ul>
                     </li>
