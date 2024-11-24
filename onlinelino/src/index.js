108f54b74a8ac6da4d51228a201a8ea9256acdcd
aluno21530 <aluno21530@ipt.pt>
2024-11-24 17:59:33 +0000

    connection between BE and FE stablished

--- onlinelino/src/index.js
+++ onlinelino/src/index.js
@@ -4,23 +4,24 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
-import ReactDOM from 'react-dom';
 import { I18nextProvider } from "react-i18next";
 import i18next from "i18next";
 import './style/Style.css';
+import { createRoot } from 'react-dom/client';
 
 i18next.init({
   interpolation: { escapeValue: false }, // React already does escaping
 });
+const domNode = document.getElementById('root');
+const root = createRoot(domNode);
 
-ReactDOM.render(
+root.render(
   <React.StrictMode>
     <I18nextProvider i18n={i18next}>
       <App />
     </I18nextProvider>
   </React.StrictMode>,
-  document.getElementById('root')
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
