c9a3a348984eb2a3379a8a627077ef1bb1ca5c0d
aluno21530 <aluno21530@ipt.pt>
2025-03-20 22:59:36 +0000

    adicionado sistema de logs no servidor e parametros dinamicos para evitar sql injection

--- onlinelino/src/Utils.js
+++ onlinelino/src/Utils.js
@@ -1,2 +1,11 @@
 export const SERVER_URL = 'http://onlinelino.ipt.pt:8080';
 export const BACKOFFICE_URL = 'backoffice';
+
+const createResponseOnSuccess = (message,token = null, error = null) => {
+    return {
+        message,
+        token,
+        error
+    };
+};
+module.exports = createResponseOnSuccess;
---
+++ onlinelino/src/server/Models/DataObjects.js
@@ -0,0 +1,64 @@
+export const HomePageDO = {
+    id: "",
+    descricao_pt: "",
+    descricao_en: "",
+    imagem: "",
+    modificado_em: ""
+}
+
+export const BibliopraphyDO = {
+    id: "",
+    descricao:"",
+    modificado_em: ""
+}
+
+export const TeamDO = {
+    id:"",
+    nome:"",
+    cargo:"",
+    modificado_em:""
+}
+
+export const ContactsDO = {
+    id:"",
+    nome:"",
+    email:"",
+    modificado_em:""
+}
+
+export const OverviewDO = {
+    id:"",
+    descricao_pt:"",
+    descricao_en:"",
+    filmes:"",
+    outros_links:"",
+    modificado_em:""
+}
+
+export const MaterialsDO = {
+    id:"",
+    descricao_pt:"",
+    descricao_en:"",
+    filmes:"",
+    outros_links:"",
+    modificado_em:""
+}
+
+export const IconicDO = {
+    id:"",
+    descricao_pt:"",
+    descricao_en:"",
+    filmes:"",
+    outros_links:"",
+}
+
+export const ObraDO = {
+    id:"",
+    nome:"",
+    data_projeto:"",
+    tipologia:"",
+    localizacao:"",
+    descricao_pt:"",
+    descricao_en:"",
+    fontes_bibliografia:""
+}
