68f5da85e089aabd5602de600751e2f2e2ea8240
aluno21530 <aluno21530@ipt.pt>
2025-05-12 20:01:00 +0100

    adicionado o get para a pagina overview

--- onlinelino/src/pages/Carreer/Materials.js
+++ onlinelino/src/pages/Carreer/Materials.js
@@ -1,99 +1,74 @@
-import React from "react";
-import NavbarHome from "../../components/NavbarHome";
-import Container from "react-bootstrap/esm/Container";
-import { useTranslation } from 'react-i18next';
+import React, { useEffect, useState } from 'react';
+import NavbarHome from '../../components/NavbarHome';
+import Container from 'react-bootstrap/Container';
+import { SERVER_URL } from '../../Utils';
 
-function Materials() {
-    const { t } = useTranslation();
+const About = () => {
+    const [overview, setOverview] = useState(null);
+    const [error, setError] = useState('');
+    const [loading, setLoading] = useState(true);
 
+    useEffect(() => {
+        const fetchOverview = async () => {
+            try {
+                const response = await fetch(`${SERVER_URL}/overview`);
+                if (!response.ok) {
+                    const err = await response.json();
+                    throw new Error(err.error);
+                }
+                const data = await response.json();
+                setOverview(data);
+            } catch (err) {
+                setError(err.message);
+            } finally {
+                setLoading(false);
+            }
+        };
 
-    const { t: b } = useTranslation('translation', { keyPrefix: 'buildingsPage.buildingsProjects' });
-    const buildingsProjects = [];
-    for (let i = 0; i < 50; i++) {
-        if (!b([i]).includes("buildingsPage.buildingsProjects")) {
-            buildingsProjects.push(b([i]));
-        }
-    }
-    // buildingsPage.videos
-    const { t: v } = useTranslation('translation', { keyPrefix: 'buildingsPage.videos' });
-    const videos = [];
-    for (let i = 0; i < 50; i++) {
-        if (!v([i]).includes("buildingsPage.videos")) {
-            videos.push(v([i]));
-        }
-    }
-    // buildingsPage.videosSubtitle
-    const { t: s } = useTranslation('translation', { keyPrefix: 'buildingsPage.videosSubtitle' });
-    const videosSubtitle = [];
-    for (let i = 0; i < 50; i++) {
-        if (!s([i]).includes("buildingsPage.videosSubtitle")) {
-            videosSubtitle.push(s([i]));
-        }
-    }
-    // buildingsPage.otherLinks
-    const { t: l } = useTranslation('translation', { keyPrefix: 'buildingsPage.otherLinks' });
-    const links = [];
-    for (let i = 0; i < 50; i++) {
-        if (!l([i]).includes("buildingsPage.otherLinks")) {
-            links.push(l([i]));
-        }
-    }
+        fetchOverview();
+    }, []);
 
-
-    /*return (
-        <>
-            <NavbarHome />
-            <br />
-            <Container>
-                <p>{t('biographyPage.about')}</p>
-            </Container>
-        </>
-    );*/
+    if (loading) return <h1>Carregando...</h1>;
+    if (error) return <h1>{error}</h1>;
 
     return (
         <>
             <NavbarHome />
             <br />
             <Container>
-                <h4>{t('buildingsPage.title')}</h4>
+                <h4>{overview?.titulo}</h4>
                 <br />
-                {buildingsProjects.map((paragraph, index) => (
-                    <p key={`proj-${index}`}>{paragraph}</p>
+                {overview?.paragrafos?.map((text, i) => (
+                    <p key={`p-${i}`}>{text}</p>
                 ))}
                 <br />
-                <h6>{t('biographyPage.v')}</h6>
+                <h6>Filmes</h6>
                 <ul>
-                    {videos.map((paragraph, index) => (
-                        <React.Fragment key={`frag1-${index}`}>
-                            <li key={`li1-${index}`}>
-                                <a href={paragraph} target="_blank" rel="noreferrer">
-                                    {paragraph}
-                                </a>
-                                <br />
-                                <span>{videosSubtitle[index]}</span>
+                    {overview?.filmes?.map((filme, i) => (
+                        <React.Fragment key={`filme-${i}`}>
+                            <li>
+                                <a href={filme.url} target="_blank" rel="noreferrer">{filme.url}</a><br />
+                                {filme.titulo && <span>[{filme.titulo}]</span>}
                             </li>
                             <br />
                         </React.Fragment>
                     ))}
                 </ul>
-                <h6>{t('biographyPage.l')}</h6>
+                <h6>Outros links</h6>
                 <ul>
-                    {links.map((paragraph, index) => (
-                        <React.Fragment key={`frag2-${index}`}>
-                            <li key={`li2-${index}`}>
-                                <a href={paragraph} target="_blank" rel="noreferrer">
-                                    {paragraph}
-                                </a>
+                    {overview?.outros_links?.map((link, i) => (
+                        <React.Fragment key={`link-${i}`}>
+                            <li>
+                                <a href={link} target="_blank" rel="noreferrer">{link}</a>
                             </li>
                             <br />
                         </React.Fragment>
                     ))}
                 </ul>
-                
             </Container>
         </>
     );
-}
+};
 
 
 export default Materials;
